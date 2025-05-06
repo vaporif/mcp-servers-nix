72a36551e7b372853e7f7ee89e10a6b1057c41a6
natsukium <tomoya.otabi@gmail.com>
2025-05-06 11:53:56 +0900

    playwright-mcp: 0.0.18 -> 0.0.20
    Diff: https://github.com/microsoft/playwright-mcp/compare/refs/tags/v0.0.18...refs/tags/v0.0.20
    
    Changelog: https://github.com/microsoft/playwright-mcp/releases/tag/v0.0.20

--- pkgs/official/playwright/default.nix
+++ pkgs/official/playwright/default.nix
@@ -6,16 +6,16 @@
 
 buildNpmPackage rec {
   pname = "playwright-mcp";
-  version = "0.0.18";
+  version = "0.0.20";
 
   src = fetchFromGitHub {
     owner = "microsoft";
     repo = "playwright-mcp";
     tag = "v${version}";
-    hash = "sha256-qn6B0mAA3jW8bAV6NsVfeklbz+unROTG4L4YOQ5OWE4=";
+    hash = "sha256-N5Lm4FuErJsZLYhRJ+GNQsVseuEZWMBJX7yURiBDBZY=";
   };
 
-  npmDepsHash = "sha256-+Kuu3l5Gzq36hqPnNCqueVYHfSW8Rww5X6POQyUQmkQ=";
+  npmDepsHash = "sha256-c64JELBEpIxzKhKNSKUwlujQhLPvhU0E/GVSi6+d5jE=";
 
   meta = {
     description = "Playwright MCP server";
