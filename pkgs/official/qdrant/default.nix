f096cf747c9da49d8d50d694105938cba783f91a
Dmytro Onypko <vaporif@proton.me>
2025-06-03 14:09:08 +0100

    upd

--- pkgs/official/qdrant/default.nix
+++ pkgs/official/qdrant/default.nix
@@ -1,9 +1,25 @@
-{ callPackage, python3Packages, lib }:
+{
+  lib,
+  fetchFromGitHub,
+  python3Packages,
+}:
 let
   pythonDeps = (import ../../../lib).mkPythonDeps { inherit lib python3Packages; fetchPypi = python3Packages.fetchPypi; };
 in
-callPackage ../../reference/generic-python.nix {
-  service = "qdrant";
+python3Packages.buildPythonApplication rec {
+  pname = "mcp-server-qdrant";
+  version = "0.7.1";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "qdrant";
+    repo = "mcp-server-qdrant";
+    rev = "v${version}";
+    hash = "sha256-O3CPq4ZGbzZQL96u1zKDYPPnWZdey6HcZa7P1CgY6J8=";
+  };
+
+  build-system = [ python3Packages.hatchling ];
+
   dependencies = with python3Packages; [
     mcp
     pydantic
@@ -11,5 +27,16 @@ callPackage ../../reference/generic-python.nix {
     fastembed
     qdrant-client
   ] ++ [ pythonDeps.fastmcp ];
+
   doCheck = false;
+
+  pythonImportsCheck = [ "mcp_server_qdrant" ];
+
+  meta = {
+    description = "Model Context Protocol Server for Qdrant";
+    homepage = "https://github.com/qdrant/mcp-server-qdrant";
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [ natsukium ];
+    mainProgram = "mcp-server-qdrant";
+  };
 }
